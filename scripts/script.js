ce12f96c9e9115eae2ff465a3546e2b48e8e68d5
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2021-05-26 16:30:34 +0300

    Collapsible Oluşturma

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded',function() {
     var modals = document.querySelectorAll('.modal');
     M.Modal.init(modals);
+
+    var articles = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(articles);
 })
